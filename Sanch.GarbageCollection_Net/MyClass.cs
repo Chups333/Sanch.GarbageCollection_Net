65d3f5f937ca6ec7e19ec0735d99dcf766d7827d
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 19:48:27 +0800

    Добавьте файлы проекта.

---
+++ Sanch.GarbageCollection_Net/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.GarbageCollection_Net
+{
+    class MyClass : IDisposable // интерфейс
+    {
+        public MyClass() //конструктор
+        {
+
+        }
+
+        ~MyClass() //деструктор - вызывается в тот момент, когда экземпляр класса будет уничтожаться. Вызвать его вручную нельзя
+        {
+            //можно определить корректное закрытие потока(пример)
+        }
+        public void Dispose()
+        {
+            //как объект будет уничтожен
+            GC.Collect();
+        }
+    }
+}
